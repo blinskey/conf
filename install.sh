c029028c4cb52d2e8e26c643a000ee3d9256caca
Ben Linskey <ben@benlinskey.com>
2015-10-04 13:14:46 -0400

    Add install script

---
+++ install.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+#
+# Minimal system setup script. Requires an Internet connection and a recent
+# version of Ubuntu. Setup can be customized using various files in the same
+# directory as this file.
+#
+# TODO:
+# - Check specific OS name and version and adjust actions accordingly.
+# - Support other types of systems.
+# - Support older verrsions of Ubuntu: install Powerline from pip and symlink.
+
+set -e
+set -u
+set -o pipefail
+
+readonly DIR="$(dirname "$(readlink -f "$0")")"
+readonly PACKAGES="${DIR}/packages"
+readonly DOTFILES="${DIR}/dotfiles"
+readonly GITHUB_REPOS="${DIR}/github-repos"
+readonly CODE_DIR="${HOME}/code"
+
+main() {
+    require_sudo
+    check_os
+    install_packages
+    install_dotfiles
+    clone_github_repos
+    configure
+
+    printf "Done.\n"
+    exit 0
+}
+
+require_sudo() {
+    if [[ $(id -u) != 0 ]]; then
+	printf "This script must be run as root.\n" >&2
+	exit 1
+    fi
+}
+
+check_os() {
+    if ! grep -i 'ID_LIKE=debian' /etc/os-release; then
+	printf "This script is only compatible with Debian-like systems.\n" >&2
+	exit 1
+    fi
+}
+
+install_packages() {
+    printf "Installing packages via apt...\n"
+    # We want the list of packages to be split into words.
+    # shellcheck disable=SC2046
+    apt-get install -y $(paste -sd' ' "$PACKAGES")
+}
+
+install_dotfiles() {
+    printf "Linking dotfiles...\n"
+    while read file; do
+	ln -sf "${DIR}/${file}" ~
+    done <"$DOTFILES"
+}
+
+clone_github_repos() {
+    printf "Cloning repos from GitHub...\n"
+    mkdir -p "$CODE_DIR"
+    pushd "$CODE_DIR" &>/dev/null
+    while read repo; do
+	local name="$(echo "${repo}" | cut -d'/' -f2)"
+	local path="${CODE_DIR}/${name}"
+	rm -rf "${path:?}"
+	git clone "https://github.com/${repo}.git" "$path"
+    done <"$GITHUB_REPOS"
+    popd &>/dev/null
+}
+
+configure() {
+    printf "Setting shell to zsh...\n"
+    chsh -s "$(which zsh)" "$(logname)"
+
+    printf "Installing zsh-syntax-highlighting plugin...\n"
+    install_zsh_syntax_highlighting
+}
+
+install_zsh_syntax_highlighting() {
+    mkdir -p "/usr/share/zsh/plugins"
+    ln -s "${HOME}/code/zsh-syntax-highlighting" "/usr/share/zsh/plugins"
+}
+
+main
