e0b75d3e1d856ff8734fde8ad0b3c79176f76f18
Ben Linskey <ben@benlinskey.com>
2015-10-25 20:01:51 -0400

    Require root in install.sh

--- install.sh
+++ install.sh
@@ -16,12 +16,15 @@ set -e
 set -u
 set -o pipefail
 
+readonly LOGIN_NAME="$(logname)"
+
 readonly DIR="$(dirname "$(readlink -f "$0")")"
 readonly PACKAGES="${DIR}/packages"
 readonly DOTFILES="${DIR}/dotfiles"
 readonly CODE_DIR="${HOME}/code"
 
 main() {
+    require_root
     check_os
     install_packages
     install_dotfiles
@@ -29,6 +32,13 @@ main() {
 
     printf "Done.\n"
     exit 0
+}
+
+require_root() {
+    if [[ $(id -u) != 0 ]]; then
+        printf "This script must be run as root.\n" >&2
+        exit 1
+    fi
 }
 
 check_os() {
@@ -42,13 +52,13 @@ install_packages() {
     printf "Installing packages via apt...\n"
     # We want the list of packages to be split into words.
     # shellcheck disable=SC2046
-    sudo apt-get install -y $(paste -sd' ' "$PACKAGES")
+    apt-get install -y $(paste -sd' ' "$PACKAGES")
 }
 
 install_dotfiles() {
     printf "Linking dotfiles...\n"
     while read file; do
-        ln -sf "${DIR}/${file}" ~
+        non_root ln -sf "${DIR}/${file}" ~
     done <"$DOTFILES"
 
     install_xfce4_terminalrc
@@ -70,8 +80,8 @@ install_zsh_syntax_highlighting() {
     local target="${CODE_DIR}/zsh-syntax-highlighting"
     clone_from_github "$repo" "$target"
 
-    sudo mkdir -p "/usr/share/zsh/plugins"
-    sudo ln -s "$target" "/usr/share/zsh/plugins"
+    mkdir -p "/usr/share/zsh/plugins"
+    ln -s "$target" "/usr/share/zsh/plugins"
 }
 
 install_powerline_fonts() {
@@ -79,6 +89,7 @@ install_powerline_fonts() {
     local target="${CODE_DIR}/powerline_fonts"
     clone_from_github "$repo" "$target"
 
+    # TODO: As root?
     "${target}/install.sh"
 }
 
@@ -91,17 +102,21 @@ clone_from_github() {
     local repo_id=$1
     local target=$2
 
-    mkdir -p "$target"
+    non_root mkdir -p "$target"
     local repo_url=https://github.com/${repo_id}.git
-    git clone "$repo_url" "$target"
+    non_root git clone "$repo_url" "$target"
 }
 
 install_xfce4_terminalrc() {
     local target_dir="${HOME}/.config/xfce4/terminal"
     local target_file="${target_dir}/terminalrc"
-    rm -rf "$target_file"
-    mkdir -p "$target_dir"
-    ln -sf "${DIR}/terminalrc" "$target_file"
+    non_root rm -rf "$target_file"
+    non_root mkdir -p "$target_dir"
+    non_root ln -sf "${DIR}/terminalrc" "$target_file"
+}
+
+non_root() {
+    sudo -u "$LOGIN_NAME" "$@"
 }
 
 main
