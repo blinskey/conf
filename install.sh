f433d9a710974d724d42e13be94f9da7a2ec21c1
Benjamin Linskey <git@linskey.org>
2019-05-18 23:55:38 -0400

    Add Gnome Terminal dconf dump

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 mkdir -p $HOME/.config/cmus
 ln -sf $PWD/other/cmusrc $HOME/.config/cmus/rc
 
+# Load Gnome Terminal settings.
+if [ -x "$(command -v dconf)" ]; then
+    dconf load /org/gnome/terminal/legacy/ < other/gnome-terminal.dconf
+fi
+
 cd home
 for f in .*; do
     if [ $f != . ] \
