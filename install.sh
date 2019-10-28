12473dbcce74efcf0a2f8d5dedbcb719b014f881
Benjamin Linskey <git@linskey.org>
2019-10-28 01:25:58 -0400

    Remove unwanted config files on install
    install.sh will now remove /etc/vimrc, /etc/virc, ~/.bash_profile, and
    various files in /etc/profile.d. This fixes various unexpected behaviors
    in Fedora.

--- install.sh
+++ install.sh
@@ -1,5 +1,34 @@
 #!/bin/sh
 
+set -eu
+
+# Delete any pre-installed ~/.bash_profile. We use a universal ~/.profile for
+# bash and ksh instead.
+rm $HOME/.bash_profile
+
+if [ $(uname) = Linux ]; then
+    echo "Running on Linux. Unwanted files in /etc will be removed."
+    echo "Running commands under sudo. You may be prompted for a password."
+
+    # Disable any pre-installed global Vim customizations.
+    if [ -f /etc/vimrc ]; then
+        sudo mv /etc/vimrc /etc/vimrc.default
+    fi
+
+    if [ -f /etc/virc ]; then
+        sudo mv /etc/virc /etc/virc.default
+    fi
+
+    if [ ! -d /etc/profile.d.defaults ]; then
+        sudo cp -r /etc/profile.d /etc/profile.d.defaults
+    fi
+
+    orig_dir=$PWD
+    cd /etc/profile.d
+    sudo rm color* vim* less* which*
+    cd $orig_dir
+fi
+
 mkdir -p $HOME/.config/cmus
 ln -sf $PWD/other/cmusrc $HOME/.config/cmus/rc
 
